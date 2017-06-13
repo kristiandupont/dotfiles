f13cfb85414b038e48b771ccfa177e08f827f3c0
Kristian Dupont <kristian@kristiandupont.com>
2017-06-13 11:00:02 +0200

    ...

---
+++ install.bat
@@ -0,0 +1,3 @@
+mklink %appdata%\Code\User\settings.json c:\repositories\dotfiles\vscode\settings.json
+mklink %appdata%\Code\User\keybindings.json c:\repositories\dotfiles\vscode\vsicons.settings.json
+mklink %appdata%\Code\User\vsicons.settings.json c:\repositories\dotfiles\vscode\vsicons.settings.json
