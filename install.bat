699b939cecd32ec638bc93e4784f0ff38d174207
Kristian Dupont <kristian@kristiandupont.com>
2017-10-31 16:30:52 +0100

    ...

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 mklink %appdata%\Code\User\settings.json c:\repositories\dotfiles\vscode\settings.json
 mklink %appdata%\Code\User\keybindings.json c:\repositories\dotfiles\vscode\keybindings.settings.json
-mklink %userprofile%\.hyper.js c:\
+mklink %userprofile%\.hyper.js c:\repositories\dotfiles\.hyper.js
